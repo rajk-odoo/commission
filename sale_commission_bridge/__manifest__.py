971b4a83df036a97b054925f8552b5ed186cb974
Rajan Kumar <rajk@odoo.com>
2024-03-14 19:01:11 +0530

    added commission and sale commission bridge module

---
+++ sale_commission_bridge/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Sale Commission Bridge",
+    "category": 'Sale/Settings/Commission',
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'summary': 'Bridge Module for sale commission' ,
+    'depends': [
+        "base",
+        "sale",
+    ],
+    'data' : [
+        "views/res_config_settings_inherit.xml",
+    ],
+    "auto_install" : True,
+}
